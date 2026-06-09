81ced50a0e60629e9534d02f6d3df476d66038e6
Brian Thomas <brian.thomas@slalom.com>
2026-06-08 19:40:10 -0500

    -Cleaned Up and Organized file structure. -All orphaned deleted -resolved conflict between readme files

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,8 @@
 import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
 import Home from '../views/Home.vue'
-import Dashboard from '../views/Dashboard.vue'
-import About from '../views/About.vue'
 
 const routes: RouteRecordRaw[] = [
   { path: '/', name: 'home', component: Home },
-  { path: '/dashboard', name: 'dashboard', component: Dashboard },
-  { path: '/about', name: 'about', component: About },
 ]
 
 export const router = createRouter({
